67fb866c3bcecf3cb7a4d10cc4a17163bf3faecb
rsoto2 <49008712+rsoto2@users.noreply.github.com>
2019-04-25 20:29:19 -0700

    Create CellPhoneFinderTest.java

---
+++ CellPhoneFinderTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+class CellPhoneFinderTest 
+{
+    @Test
+    public void registerTag(List<String> a)
+    {
+        a = new ArrayList<String>();
+        List<String> expected = new ArrayList<String>();
+                
+        a.add("Owner: Pickles, Toe");
+        a.add("ID: 8675309");
+        a.add("Location: Within phone proximity");
+        
+        expected.add("Owner: Pickles, Toe");
+        expected.add("ID: 8675309");
+        expected.add("Location: Within phone proximity");
+        
+        // Test to see if generated List matches the expected List
+        assertEquals(a, expected);
+    }
+
+}
