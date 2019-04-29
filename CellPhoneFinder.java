868015fab94f579e9a0c3f541ad9e0f2905c4960
Dat Boi <thatboy@yeet.com>
2019-04-28 22:23:01 -0700

    yuh

--- CellPhoneFinder.java
+++ CellPhoneFinder.java
@@ -1,41 +1,31 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
-public class CellPhoneFinder 
-{
-        
-        public static void registerTag(List<String> a) throws IOException
-        {
-            /*This method will create a file that contains
-             *important user info like the owner's name, tag number, 
-             *and an empty string that might be filled by another method?
-             *Once this location is changed by one of us in some
-             *other method...or something...the flag should change?
-             *Let me know how you guys feel about this.
-             */
-            
-            a.add("Owner: Pickles, Toe");
-            a.add("ID: 8675309");
-            a.add("Location: Within phone proximity");
-            
-            FileOutputStream fWrite = new FileOutputStream("customerData.txt");
-            ObjectOutputStream fObject = new ObjectOutputStream(fWrite);
-            fObject.writeObject(a);
-            fObject.close();
-            
-            for(String s: a)
-            {
-                System.out.println(s);
-            }
-            
-        }
-        
-        public static void main(String args[]) throws IOException
-        {
-            List<String> tagInfo = new ArrayList<String>();
-            registerTag(tagInfo);
+public class CellPhoneFinder {
+    private static ArrayList<Tile> accounts = new ArrayList<Tile>();
+
+    public static void registerTag(int id, String owner, String location) throws IOException {
+        accounts.add(new Tile(id, owner, location));
+    }
+
+    public static void retrieveTag(int id) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (id == accounts.get(i).id)
+                accounts.get(i).print();
         }
     }
+
+    public static void addUsers(int id, String name) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (id == accounts.get(i).id)
+                accounts.get(i).otherUsers.add(name);
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        registerTag(8675309, "Pickles, Toe", "Within phone proximity");
+        retrieveTag(8675309);
+        addUsers(8675309, "my guy");
+        retrieveTag(8675309);
+    }
+}
